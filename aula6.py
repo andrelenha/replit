8ba577da8d465e200e740dc2ea3999a0a0e4a09b
Andre <andrelenha@gmail.com>
2022-12-14 17:48:19 +0000

    aula6

---
+++ aula6.py
@@ -0,0 +1,8 @@
+print("Login Seguro")
+username = input ("Usuário:")
+if username == "mark":
+    print("Bem vindo",username," !")
+elif username == "suzanne":
+    print("Como vai", username,"!!")
+else:
+    print("Dê o fora!?!?")
