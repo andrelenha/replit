f9ee4e7ca10097440393079cd3cadf6c5cd1118b
Andre <andrelenha@gmail.com>
2022-12-14 15:21:05 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -1,8 +1,12 @@
-print("Login Seguro")
-username = input ("Usuário:")
-if username == "mark":
-    print("Bem vindo",username," !")
-elif username == "suzanne":
-    print("Como vai", username,"!!")
+print("Secure Login")
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "David" and password == "PyTh0nR*cks":
+  print("Welcome, David! You are looking nice today!")
+elif username == "Becky" and password == "Repl!t4evEr":
+  print("Hi Becky! Love your hair today!")
+elif username == "Bill" and password == "SmashTHEb*gs!":
+  print("Yo! Bill! What up?!")
 else:
-    print("Dê o fora!?!?")
+  print("You do not have access. Go away!")
